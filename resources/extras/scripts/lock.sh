e27c0331f29fb0b8c0f5c0de795db5980c9409cf
infraflakes <infraflakes@proton.me>
2026-04-03 17:13:48 +0700

    Remove redundant artifacts

--- resources/extras/scripts/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Blur-lock: screenshot, blur, lock
-grim -l 0 /tmp/lockscreen.png
-ffmpeg -y -i /tmp/lockscreen.png -vf "boxblur=8:2" /tmp/lockblur.png 2>/dev/null
-swaylock -f -i /tmp/lockblur.png
